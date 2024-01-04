2f3d9c50d9d50153b14d9d710add985f8e3717f3
stukenvitalii <stukenvitalii@gmail.com>
2024-01-04 16:15:50 +0300

    Add deleting, editing feature. Add modal window for confirming deleting

--- PetProjects/TaskManager/src/main/java/com/example/TaskManager/entity/Task.java
+++ PetProjects/TaskManager/src/main/java/com/example/TaskManager/entity/Task.java
@@ -1,11 +1,7 @@
 package com.example.TaskManager.entity;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
 
-@Setter
-@Getter
 @Entity
 @Table(name = "tasks")
 public class Task {
@@ -35,4 +31,35 @@ public class Task {
         this.status = status;
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
