9a933e9d62f22ecae09016689acd5e0619054265
stukenvitalii <stukenvitalii@gmail.com>
2024-04-22 22:50:46 +0300

    use Lombok annotations

--- PetProjects/TaskManager/src/main/java/com/example/TaskManager/entity/Task.java
+++ PetProjects/TaskManager/src/main/java/com/example/TaskManager/entity/Task.java
@@ -1,11 +1,18 @@
 package com.example.TaskManager.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 @Table(name = "tasks")
 public class Task {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name="task_id")
@@ -19,47 +26,4 @@ public class Task {
 
     @Column(name = "task_status")
     private String status;
-
-    public Task() {
-
-    }
-
-    public Task(String name,String description,String status) {
-        super();
-        this.name = name;
-        this.description = description;
-        this.status = status;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
 }
