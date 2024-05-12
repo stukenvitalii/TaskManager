b9647df74f9dae3a33908b0b972076626bce4cd1
stukenvitalii <stukenvitalii@gmail.com>
2024-05-12 16:37:32 +0300

    add User entity

---
+++ PetProjects/TaskManager/src/main/java/com/example/TaskManager/entity/User.java
@@ -0,0 +1,26 @@
+package com.example.TaskManager.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="user_id")
+    private Long id;
+
+    @Column(name = "user_name",nullable = false)
+    private String username;
+
+    @Column(name = "user_password", nullable = false)
+    private String password;
+}
