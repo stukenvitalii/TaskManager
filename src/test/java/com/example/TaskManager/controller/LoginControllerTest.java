fbe2dc38a8fd2a2b2bfdf0a27ce442cd528aaf6c
stukenvitalii <stukenvitalii@gmail.com>
2024-09-26 18:48:16 +0300

    add tests

--- src/test/java/com/example/TaskManager/controller/LoginControllerTest.java
+++ src/test/java/com/example/TaskManager/controller/LoginControllerTest.java
@@ -10,18 +10,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.ui.Model;
-import org.springframework.web.context.WebApplicationContext;
 
 import java.util.Optional;
 
 import static org.mockito.ArgumentMatchers.anyString;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(LoginController.class)
 public class LoginControllerTest {
-
+    //TODO fix test
     @Autowired
     private MockMvc mockMvc;
 
