2fa6eed79fbf2fbffbdb0fc524aaf4e74ccde756
stukenvitalii <stukenvitalii@gmail.com>
2024-09-26 18:51:06 +0300

    update CustomUserDetailsService

--- src/main/java/com/example/TaskManager/service/impl/CustomUserDetailsService.java
+++ src/main/java/com/example/TaskManager/service/impl/CustomUserDetailsService.java
@@ -2,12 +2,14 @@ package com.example.TaskManager.service.impl;
 
 import com.example.TaskManager.entity.User;
 import com.example.TaskManager.repository.UserRepository;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
+import java.util.Collections;
 
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
@@ -21,10 +23,13 @@ public class CustomUserDetailsService implements UserDetailsService {
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         User user = userRepository.findByUsername(username)
                 .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        GrantedAuthority authority = new SimpleGrantedAuthority(user.getRole().getName());
+
         return new org.springframework.security.core.userdetails.User(
                 user.getUsername(),
                 user.getPassword(),
-                new ArrayList<>()
+                Collections.singletonList(authority)
         );
     }
 }
